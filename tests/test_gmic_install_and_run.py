2a986d8541603c8457f3a66ac8d006d335de61ae
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-11-07 15:10:06 +0100

    tests: fixing pytest collecting freezes by moving to /tmp instead, adding pip gmic-py package url or name injectability defaulting to Github URL for now

--- tests/test_gmic_install_and_run.py
+++ tests/test_gmic_install_and_run.py
@@ -4,7 +4,8 @@ def test_install_gmic_py():
     # per https://github.com/jgonggrijp/pip-review/issues/44#issuecomment-277720359
     import subprocess
     import sys
-    exit_code = subprocess.call([sys.executable, '-m', 'pip', 'install', 'gmic-py'])
+    import os
+    exit_code = subprocess.call([sys.executable, '-m', 'pip', 'install', os.environ.get('GMIC_PY_PIP_PKG', 'gmic-py'), '--no-cache-dir'])
     assert exit_code == 0
 
 def test_import_gmic_py():
@@ -13,21 +14,24 @@ def test_import_gmic_py():
 def test_run_gmic_cli_helloworld(capsys):
     import gmic_py
     # Using pytest stderr capture per https://docs.pytest.org/en/latest/capture.html#accessing-captured-output-from-a-test-function
-    captured = capsys.readouterr()
-    result = gmic_py.run('echo "hello world"')
-    assert "[gmic]-0./ Start G'MIC interpreter." in captured
-    assert "hello world" in captured
+    captured = capsys.readout()
+    result = gmic_py.run('v - echo_stdout "hello world"')
+    assert "hello world\n" in captured
 
 def test_run_gmic_cli_simple_3pixels_png_output():
     import gmic_py
     import pathlib
     png_filename = "a.png"
-    gmic_py.run('input "(1,2,3)" -output ' + png_filename)
+    gmic_py.run('input "(0,128,255)" -output ' + png_filename)
     a_png = pathlib.Path(png_filename)
     # Ensure generated png file exists and is non empty
     assert a_png.exists()
     assert a.png.stat().st_size > 0
     a_png.unlink()
+
+# todo: test with an empty input image list
+
+# todo: test output pythonized objects (ex: 3 pixels raw image buffer)
 
 # todo: test exceptions raising
 
