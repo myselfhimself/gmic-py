c432bef5c468e90c1d5541f359333c90026e9462
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-11-07 16:10:48 +0100

    tests: fixing gmic module import names, fixing pytest standard output retrieval

--- tests/test_gmic_install_and_run.py
+++ tests/test_gmic_install_and_run.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_install_gmic_py():
+def test_install_gmic():
     # per https://github.com/jgonggrijp/pip-review/issues/44#issuecomment-277720359
     import subprocess
     import sys
@@ -8,21 +8,21 @@ def test_install_gmic_py():
     exit_code = subprocess.call([sys.executable, '-m', 'pip', 'install', os.environ.get('GMIC_PY_PIP_PKG', 'gmic'), '--no-cache-dir'])
     assert exit_code == 0
 
-def test_import_gmic_py():
-    import gmic_py
+def test_import_gmic():
+    import gmic
 
 def test_run_gmic_cli_helloworld(capsys):
     import gmic
     # Using pytest stderr capture per https://docs.pytest.org/en/latest/capture.html#accessing-captured-output-from-a-test-function
-    captured = capsys.readout()
-    result = gmic_py.run('v - echo_stdout "hello world"')
-    assert "hello world\n" in captured
+    gmic.run('v - echo_stdout "hello world"')
+    out, err = capsys.readouterr()
+    assert "hello world" == out
 
 def test_run_gmic_cli_simple_3pixels_png_output():
     import gmic
     import pathlib
     png_filename = "a.png"
-    gmic_py.run('input "(0,128,255)" -output ' + png_filename)
+    gmic.run('input "(0,128,255)" -output ' + png_filename)
     a_png = pathlib.Path(png_filename)
     # Ensure generated png file exists and is non empty
     assert a_png.exists()
