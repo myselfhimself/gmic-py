ca764ef0a0d61251065a582193d8a46a73d930da
Jonathan-David Schroder <jonathan.schroder@gmail.com>
2019-11-19 19:56:33 +0100

    Github Workflow: trying to make tests runnable on cpython 2

--- cpython/makegmicpymodule.sh
+++ cpython/makegmicpymodule.sh
@@ -1,9 +1,1 @@
 g++ gmicpy.cpp -o gmicpy.so -I . -L /usr/lib/i386-linux-gnu/ -L . -lgmic -std=c++11 -shared $INCLUDES $LIBS
-
-ln -s libgmic.so libgmic.so.2
-
-LD_LIBRARY_PATH=. ldd gmicpy.so
-LD_LIBRARY_PATH=. python3 testgmicpy.py
-
-
-
--- cpython/testgmicpy.py
+++
@@ -1,3 +0,0 @@
-import gmicpy
-a = gmicpy.run("echo_stdout \"hello world\"");
-print(a)
