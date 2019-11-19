5a1a4f4a887b3845a8492b7667d9bb3ef5dbb126
Jonathan-David Schroder <jonathan.schroder@gmail.com>
2019-11-19 19:32:14 +0100

    Github Workflow: tentative dynamic libs and includes path for cpython compiling

--- cpython/makegmicpymodule.sh
+++ cpython/makegmicpymodule.sh
@@ -1,4 +1,4 @@
-g++ gmicpy.cpp -o gmicpy.so -I /usr/include/python3.5m/ -I . -L /usr/lib/i386-linux-gnu/ -L . -lpython3.5m -lgmic -std=c++11 -shared
+g++ gmicpy.cpp -o gmicpy.so -I . -L /usr/lib/i386-linux-gnu/ -L . -lgmic -std=c++11 -shared $INCLUDES $LIBS
 
 ln -s libgmic.so libgmic.so.2
 
