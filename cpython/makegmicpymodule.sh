f695c3b0d0935ef3e3cd4e4eae57fc9d1bbdbf49
Jonathan-David Schroder <jonathan.schroder@gmail.com>
2019-11-19 20:28:09 +0100

    Github Workflow: trying to make tests runnable on cpython 6

--- cpython/makegmicpymodule.sh
+++ cpython/makegmicpymodule.sh
@@ -1,1 +1,2 @@
+echo WILL COMPILE WITH g++ gmicpy.cpp -o gmicpy.so -I . -L /usr/lib/i386-linux-gnu/ -L . -lgmic -std=c++11 -shared $INCLUDES $LIBS
 g++ gmicpy.cpp -o gmicpy.so -I . -L /usr/lib/i386-linux-gnu/ -L . -lgmic -std=c++11 -shared $INCLUDES $LIBS
