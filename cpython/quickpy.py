4bc8b053d252305ef2f4bce048b91d1476160608
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-11-20 15:31:44 +0100

    Cpython: Fix breakout quicktest

--- cpython/quickpy.py
+++ cpython/quickpy.py
@@ -2,7 +2,7 @@ import sys
 print(sys.path)
 import os
 print(os.environ)
-print(os.environ["PYTHON_PATH"])
+print(os.environ["PYTHONPATH"])
 print(os.environ["LD_LIBRARY_PATH"])
 import gmicpy
 print(gmicpy)
