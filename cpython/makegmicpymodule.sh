275341d986cd306c669a1a0c3fcb561a73050361
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-11-20 13:00:36 +0100

    Github workflow: cpython: tentative .so compilation fix with -fPIC

--- cpython/makegmicpymodule.sh
+++ cpython/makegmicpymodule.sh
@@ -1,2 +1,7 @@
-echo WILL COMPILE WITH g++ gmicpy.cpp -o gmicpy.so -I . -L /usr/lib/i386-linux-gnu/ -L . -lgmic -std=c++11 -shared $INCLUDES $LIBS
-g++ gmicpy.cpp -o gmicpy.so -I . -L /usr/lib/i386-linux-gnu/ -L . -lgmic -std=c++11 -shared $INCLUDES $LIBS
+#!/bin/sh
+
+COMPILE_COMMAND="g++ -fPIC gmicpy.cpp -o gmicpy.so -I . -L /usr/lib/i386-linux-gnu/ -L . -lgmic -std=c++11 -shared $INCLUDES $LIBS"
+
+echo WILL COMPILE WITH $COMPILE_COMMAND
+
+$COMPILE_COMMAND
