e4d14502bd51e75640b1c2f5b53fee5ee9c80f00
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-11-26 11:31:22 +0100

    cpython: renaming Cpython module from gmicpy to gmic, adding __init__ file to fix setup.py build

--- cpython/gmicpy.cpp
+++ cpython/gmicpy.cpp
@@ -10,19 +10,19 @@ PyObject* run_impl(PyObject*, PyObject* commands_line)
   return Py_True;
 }
 
-static PyMethodDef gmicpy_methods[] = {
+static PyMethodDef gmic_methods[] = {
   {"run", (PyCFunction)run_impl, METH_O, nullptr },
   {nullptr, nullptr, 0, nullptr }
 };
 
-PyModuleDef gmicpy_module = {
+PyModuleDef gmic_module = {
   PyModuleDef_HEAD_INIT,
   "gmic",
   "Gmic Python binding",
   0,
-  gmicpy_methods
+  gmic_methods
 };
 
-PyMODINIT_FUNC PyInit_gmicpy() {
-  return PyModule_Create(&gmicpy_module);
+PyMODINIT_FUNC PyInit_gmic() {
+  return PyModule_Create(&gmic_module);
 }
