dc41035e2268f23d8c94e3bbf38857d0443d0039
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-05 18:31:10 +0100

    cpython: for .so test, readding LD_LIBRARY_PATH to here

--- cpython/3_test_compiled_so.sh
+++ cpython/3_test_compiled_so.sh
@@ -1,1 +1,1 @@
-pip3 uninstall gmic -y; cd ./build/lib.linux-x86_64-*/ ; pip3 install -r ../../tests/requirements.txt ; pwd; ls; python3 -m pytest ../../tests/test_gmic_install_and_run.py ; cd ../..
+pip3 uninstall gmic -y; cd ./build/lib.linux-x86_64-*/ ; LD_LIBRARY_PATH=. ; pip3 install -r ../../tests/requirements.txt ; pwd; ls; python3 -m pytest ../../tests/test_gmic_install_and_run.py ; cd ../..
