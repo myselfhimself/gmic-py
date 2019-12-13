c184bb9d68549590c08a6d5321bd22e070bf1ee8
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-13 18:16:27 +0100

    cpython: github workflow: tentative adding all manylinux architectures

---
+++ cpython/33_build_manylinux.sh
@@ -0,0 +1,21 @@
+# Run this from the cpython/ directory only :)
+
+# Feel free to preset the following variables before running this script
+# Default values allow for local running on a developer machine :)
+if [ -z "$DOCKER_IMAGE" ]
+then
+  DOCKER_IMAGE="quay.io/pypa/manylinux1_x86_64"
+fi
+if [ -z "$PLAT" ]
+then
+  PLAT="manylinux1_x86_64"
+fi
+if [ -z "$PRE_CMD" ]
+then
+  PRE_CMD=
+fi
+
+docker pull $DOCKER_IMAGE
+docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE find /io
+docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /bin/bash /io/travis/build-wheels.sh
+ls wheelhouse/
--- cpython/33_build_manylinux1.sh
+++
@@ -1,9 +0,0 @@
-# Run this from the cpython/ directory only :)
-DOCKER_IMAGE="quay.io/pypa/manylinux1_x86_64"
-PLAT="manylinux1_x86_64"
-PRE_CMD=
-
-docker pull $DOCKER_IMAGE
-docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE find /io
-docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /bin/bash /io/travis/build-wheels.sh
-ls wheelhouse/
