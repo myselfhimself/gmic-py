c9cebb4c8450d15d61927633a9588bb8d6ba5eb0
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-13 17:17:46 +0100

    cpython: pypi: skip twine upload if file already uploaded

--- cpython/11_send_to_pypi.sh
+++ cpython/11_send_to_pypi.sh
@@ -3,16 +3,16 @@ export TWINE_USERNAME=__token__
 export TWINE_PASSWORD=pypi-AgEIcHlwaS5vcmcCJGE0M2VjNGQ1LWIxNjYtNGU3Yy1iYTliLTgwNTU3ZjA2MDY5YwACNXsicGVybWlzc2lvbnMiOiB7InByb2plY3RzIjogWyJnbWljIl19LCAidmVyc2lvbiI6IDF9AAAGIMGxJOG-7GYZAKfYWozw0gq5wtTRxUWSSd605rMOj53-
 export TWINE_REPOSITORY_URL=https://upload.pypi.org/legacy/
 TWINE=twine
+TWINE_OPTIONS="--skip-existing"
 
 if [ -d "dist/" ]; then
   for a in `ls dist/`; do
-    $TWINE upload $a
+    $TWINE $TWINE_OPTIONS upload $a
   done
 fi
 
 if [ -d "wheelhouse/" ]; then
   for a in `ls wheelhouse/*manylinux*`; do
-    $TWINE upload $a
+    $TWINE $TWINE_OPTIONS upload $a
   done
 fi
-
