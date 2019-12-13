b8d7d408c630e16d9d740067376758e643bc1f5a
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-13 17:00:57 +0100

    cpython: fixing twine path

--- cpython/11_send_to_pypi.sh
+++ cpython/11_send_to_pypi.sh
@@ -2,7 +2,7 @@ pip install twine
 export TWINE_USERNAME=__token__
 export TWINE_PASSWORD=pypi-AgEIcHlwaS5vcmcCJGE0M2VjNGQ1LWIxNjYtNGU3Yy1iYTliLTgwNTU3ZjA2MDY5YwACNXsicGVybWlzc2lvbnMiOiB7InByb2plY3RzIjogWyJnbWljIl19LCAidmVyc2lvbiI6IDF9AAAGIMGxJOG-7GYZAKfYWozw0gq5wtTRxUWSSd605rMOj53-
 export TWINE_REPOSITORY_URL=https://upload.pypi.org/legacy/
-TWINE=/export/home/schrode191/.local/bin/twine
+TWINE=twine
 
 if [ -d "dist/" ]; then
   for a in `ls dist/`; do
