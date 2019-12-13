887856c933646f24eb4dc372a9dc3e2e08c6f8e0
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-13 16:39:13 +0100

    cpython: github workflow: circumventing non-wildcard paths expansion in send_to_pypi step

--- cpython/11_send_to_pypi.sh
+++ cpython/11_send_to_pypi.sh
@@ -2,8 +2,17 @@ pip install twine
 export TWINE_USERNAME=__token__
 export TWINE_PASSWORD=pypi-AgEIcHlwaS5vcmcCJGE0M2VjNGQ1LWIxNjYtNGU3Yy1iYTliLTgwNTU3ZjA2MDY5YwACNXsicGVybWlzc2lvbnMiOiB7InByb2plY3RzIjogWyJnbWljIl19LCAidmVyc2lvbiI6IDF9AAAGIMGxJOG-7GYZAKfYWozw0gq5wtTRxUWSSd605rMOj53-
 export TWINE_REPOSITORY_URL=https://upload.pypi.org/legacy/
-TWINE=twine
+TWINE=/export/home/schrode191/.local/bin/twine
 
-[ -d "dist/" ] && $TWINE upload dist/*
-[ -d "wheelhouse/" ] && $TWINE upload wheelhouse/*manylinux*
+if [ -d "dist/" ]; then
+  for a in `ls dist/`; do
+    $TWINE upload $a
+  done
+fi
+
+if [ -d "wheelhouse/" ]; then
+  for a in `ls wheelhouse/*manylinux*`; do
+    $TWINE upload $a
+  done
+fi
 
