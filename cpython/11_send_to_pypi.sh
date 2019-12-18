3e26eae70c1cf0db2be7643318bb26242a2f16c4
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-18 18:46:46 +0100

    cpython: fix pypi twine step

--- cpython/11_send_to_pypi.sh
+++ cpython/11_send_to_pypi.sh
@@ -19,7 +19,7 @@ if [ -d "dist/" ]; then
 fi
 
 # Upload binary python wheels if found
-if [ -d "wheelhouse/" ]; then
+if [ -d "wheelhouse/*" ]; then # Keep the * after /, it is very important!
   for a in `ls wheelhouse/ | grep -E 'manylinux|macosx'`; do
     $TWINE upload $a $TWINE_OPTIONS
   done
