3163b5db94fdb89e175c73aef107800eb11e6bf5
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-18 14:59:18 +0100

    cpython: github workflow: all steps: making file listing POSIX compatible

--- cpython/00_all_steps.sh
+++ cpython/00_all_steps.sh
@@ -1,4 +1,4 @@
 source 1_clean_and_regrab_gmic_src.sh && source 2_compile.sh && source 3_test_compiled_so.sh
 source 4_build_wheel.sh && source 5_test_wheel.sh && source 6_build_sdist.sh && source 7_test_sdist.sh
 echo "This is the final file tree:"
-find
+find .
