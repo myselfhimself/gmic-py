4f6cc972b493b5c2b310f71c63ccc57369ffc0ff
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-19 15:22:58 +0100

    cpython: pytests: openmp: more insight into actual gmic command output value

--- cpython/tests/test_gmic_install_and_run.py
+++ cpython/tests/test_gmic_install_and_run.py
@@ -15,9 +15,18 @@ def test_catch_exceptions():
 @pytest.mark.xfail(raises=AssertionError, reason="If openmp fails to be found, gmic usually falls back and runs more slowly")
 def test_run_gmic_ensure_openmp_linked_and_working(capfd):
     import gmic
+    import traceback
     gmic.run('v - sp lena eval. "end(call(\'echo_stdout[] \',merge(t,max)))"')
     outerr = capfd.readouterr()
-    assert int(outerr.out.strip()) > 0 # should show "0\n" if openmp not working
+    try:
+        assert int(outerr.out.strip()) > 0 # should show "0\n" if openmp not working
+    except AssertionError:
+        # Traceback display code from https://stackoverflow.com/a/11587247/420684
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb) # Fixed format
+        tb_info = traceback.extract_tb(tb)
+        filename, line, func, text = tb_info[-1]
+        pytest.xfail('parallel test case fails, OpenMP probably could not link or compile well on this platform, gmic parallelization will not work: ' + text)
 
 def test_run_gmic_cli_helloworld(capfd):
     import gmic
