d518ca9530d5b24c5f7e37b200ab28bc05937d96
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-19 15:00:12 +0100

    cpython: pypi upload: more verbose console output(2)

--- cpython/11_send_to_pypi.sh
+++ cpython/11_send_to_pypi.sh
@@ -1,3 +1,4 @@
+set -x
 pip install twine
 export TWINE_USERNAME=__token__
 export TWINE_PASSWORD=pypi-AgEIcHlwaS5vcmcCJGE0M2VjNGQ1LWIxNjYtNGU3Yy1iYTliLTgwNTU3ZjA2MDY5YwACNXsicGVybWlzc2lvbnMiOiB7InByb2plY3RzIjogWyJnbWljIl19LCAidmVyc2lvbiI6IDF9AAAGIMGxJOG-7GYZAKfYWozw0gq5wtTRxUWSSd605rMOj53-
