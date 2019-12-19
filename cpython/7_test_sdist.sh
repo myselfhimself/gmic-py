972b156cb87b8124fa8e16940f4c3977b14554da
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-19 15:52:09 +0100

    cpython: linuxes: adding pytest verbosity to sdist testing

--- cpython/7_test_sdist.sh
+++ cpython/7_test_sdist.sh
@@ -1,3 +1,3 @@
 $PIP3 install dist/gmic*.tar.gz --verbose
-$PYTHON3 -m pytest tests/test_gmic_install_and_run.py
+$PYTHON3 -m pytest tests/test_gmic_install_and_run.py -vvv -rxXs
 $PIP3 uninstall gmic -y
