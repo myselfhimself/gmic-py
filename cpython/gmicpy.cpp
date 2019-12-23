f0c22b71d35d3d5cb692338fc6ab63f38d75952d
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2019-12-23 14:36:28 +0100

    Cpython : more gmicimage binding

--- cpython/gmicpy.cpp
+++ cpython/gmicpy.cpp
@@ -14,6 +14,76 @@ typedef struct {
     gmic_image<T> * ptrObj;
 } PyGmicImage;
 
+static int PyGmicImage_init(PyGmicImage<T> *self, PyObject *args, PyObject *kwds)
+// initialize PyGmicImage Object
+{
+    unsigned int _width;       // Number of image columns (dimension along the X-axis)
+    unsigned int _height;      // Number of image lines (dimension along the Y-axis)
+    unsigned int _depth;       // Number of image slices (dimension along the Z-axis)
+    unsigned int _spectrum;    // Number of image channels (dimension along the C-axis)
+  
+    // SKIPPED FOR NOW
+    bool _is_shared = false;           // Tells if the data buffer is shared by another structure
+    T *_data;                  // Pointer to the first pixel value
+
+
+    if (! PyArg_ParseTuple(args, "IIII", &_width, &_height, &_depth, &_spectrum))
+        return -1;
+
+    self->ptrObj=new gmic_image<T>();
+    self->ptrObj->_is_shared = _is_shared;
+    self->ptrObj->_data = NULL;
+    self->ptrObj->assign(_width, _height, _depth, _spectrum);
+
+    return 0;
+}
+
+
+
+
+
+
+
+static void PyGmicImage_dealloc(PyGmicImage<T> * self)
+// destruct the object
+{
+    free(self->ptrObj->_data);
+    delete self->ptrObj;
+    Py_TYPE(self)->tp_free(self);
+
+}
+
+
+
+static PyObject * PyGmicImage_from_numpy_array(PyGmicImage<T>rray(PyGmicImage<T>* self, PyObject* args)
+{
+
+
+// todo array of floats instead from numpy array
+    if (! PyArg_ParseTuple(args, "f", &self->objPtr->_data))
+        return Py_False;
+
+    retval = (self->ptrObj)->_data != NULL;
+
+    return Py_BuildValue("i",retval);
+}
+
+
+
+static PyTypeObject PyGmicImageType = { PyVarObject_HEAD_INIT(NULL, 0)
+                                    "gmic.GmicImage"   /* tp_name */
+                                };
+
+static PyMethodDef PyGmicImage_methods[] = {
+    { "from_numpy_array", (PyCFunction)PyGmicImage_from_numpy_array,    METH_VARARGS,       "get numpy array's data" },
+    {NULL}  /* Sentinel */
+};
+
+
+
+
+
+
 PyObject* run_impl(PyObject*, PyObject* commands_line) 
 {
   const char* c_commands_line = PyUnicode_AsUTF8(commands_line);
@@ -27,6 +97,8 @@ PyObject* run_impl(PyObject*, PyObject* commands_line)
   }
   return Py_True;
 }
+
+
 
 static PyMethodDef gmic_methods[] = {
   {"run", (PyCFunction)run_impl, METH_O, nullptr },
@@ -42,5 +114,27 @@ PyModuleDef gmic_module = {
 };
 
 PyMODINIT_FUNC PyInit_gmic() {
-  return PyModule_Create(&gmic_module);
+  
+    
+    PyObject* m;
+
+    PyGmicImageType.tp_new = PyType_GenericNew;
+    PyGmicImageType.tp_basicsize=sizeof(PyGmicImage);
+    PyGmicImageType.tp_dealloc=(destructor) PyGmicImage_dealloc;
+    PyGmicImageType.tp_doc="Gmic Image buffer";
+    PyGmicImageType.tp_methods=PyGmicImage_methods;
+
+    PyGmicImageType.tp_init=(initproc)PyGmicImage_init;
+
+    if (PyType_Ready(&PyGmicImageType) < 0)
+        return NULL;
+
+    m = PyModule_Create(&gmic_module);
+    if (m == NULL)
+        return NULL;
+
+    Py_INCREF(&PyGmicImageType);
+    PyModule_AddObject(m, "GmicImage", (PyObject *)&PyGmicImageType); // Add GmicImage object to the module
+    return m;
+    
 }
