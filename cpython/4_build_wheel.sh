4432bedf1e9b5c761dd8699d8ef9197750b838c8
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-01-13 17:56:19 +0100

    cpython: setup.py compiling: disabling X11, libcurl, libpng if not installed

--- cpython/4_build_wheel.sh
+++ cpython/4_build_wheel.sh
@@ -1,2 +1,3 @@
 $PIP3 install wheel
 $PYTHON3 setup.py bdist_wheel
+# We are doing no auditwheel repair step here, this is for home made local development :)
--- cpython/setup.py
+++ cpython/setup.py
@@ -13,19 +13,38 @@ here = path.abspath(path.dirname(__file__))
 gmic_src_path = path.abspath('src/gmic/src')
 
 # List of libs to get include directories and linkable libraries paths from for compiling
-pkgconfig_list = ['zlib', 'fftw3', 'libpng']
+pkgconfig_list = ['zlib']
+# Only require x11 if found
+cimg_use_curl_enabled = '0'
+if pkgconfig.exists('x11'):
+    cimg_display_enabled = '1'
+    pkgconfig_list += ['x11']
 
-# Disable libcurl and the related compilation macro only on linux 64bit and 32bit targets
-cimg_use_curl_enabled = None # '1' by default
-if sys.platform == 'linux':# and platform.architecture()[0] == '64bit':
-    cimg_use_curl_enabled = False
-else:
+# Only require libpng if found
+cimg_use_png_enabled = '0'
+if pkgconfig.exists('libpng'):
+    cimg_use_png_enabled = '1'
+    pkgconfig_list += ['libpng']
+
+# Only require fftw3 if found (non-2^ size image processing fails without it)
+cimg_use_fftw3_enabled = '0'
+if pkgconfig.exists('fftw3'):
+    cimg_use_fftw3_enabled = '1'
+    pkgconfig_list += ['fftw3']
+
+# Disable libcurl only on manylinuxes, because of a buggy audithweel repair constraint
+# See https://github.com/pypa/manylinux/issues/411
+# On any other platform, use libcurl if installed, else G'MIC falls back nicely to installed curl executable
+cimg_use_curl_enabled = '0'
+if 'manylinux' in environ.get('PLAT',''):
+    cimg_use_curl_enabled = '0'
+elif pkgconfig.exists('libcurl'):
     cimg_use_curl_enabled = '1'
     pkgconfig_list += ['libcurl']
 
 packages = pkgconfig.parse(" ".join(pkgconfig_list))
 libraries = packages['libraries'] + ['pthread'] # removed core-dumping 'gomp' temporarily (for manylinux builds)
-libraries += ['X11'] if sys.platform != 'darwin' else [] # disable X11 linking on MacOS permanently
+
 library_dirs = packages['library_dirs'] + [here, gmic_src_path]
 if sys.platform == 'darwin':
     library_dirs += ['/usr/local/opt/llvm@6/lib']
@@ -42,10 +61,7 @@ elif sys.platform == 'linux': # Enable openmp for 32bit & 64bit linuxes
     extra_compile_args += ['-fopenmp']
     extra_link_args += ['-lgomp']
 
-cimg_display_enabled = str(int(sys.platform != 'darwin')) # Disable any X display on MacOS, value is '1' or '0'
-define_macros = [('gmic_build', None), ('cimg_use_png', None), ('cimg_date', '""'), ('cimg_time', '""'), ('gmic_is_parallel', None), ('cimg_use_zlib', None), ('cimg_display', cimg_display_enabled)]
-if cimg_use_curl_enabled is not False:
-    define_macros += [('cimg_use_curl', cimg_use_curl_enabled)]
+define_macros = [('gmic_build', None), ('cimg_use_png', cimg_use_png_enabled), ('cimg_date', '""'), ('cimg_time', '""'), ('gmic_is_parallel', None), ('cimg_use_zlib', None), ('cimg_display', cimg_display_enabled), ('cimg_use_curl', cimg_use_curl_enabled), ('cimg_use_fftw3', cimg_use_fftw3_enabled)]
 
 print("Define macros:")
 print(define_macros)
