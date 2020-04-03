2f4d42bcc01d300ae618232a4474784b18e31e42
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-04-03 18:57:55 +0200

    #35 #14 macosx python ppm pytest file format troubleshooting(3)

--- tests/test_gmic_py.py
+++ tests/test_gmic_py.py
@@ -79,7 +79,7 @@ def test_run_gmic_instance_run_ppm_vs_pure_python_ppm_equality(gmic_instance_run
     width = 256
     height = 128
     maxval = 255
-    ppm_header = 'P6 {} {} {}\n'.format(width, height, maxval)
+    ppm_header = 'P6\n{} {}\n{}\n'.format(width, height, maxval)
     
     # PPM image data (filled with blue)
     image = array.array('B', [0, 0, 255] * width * height)
