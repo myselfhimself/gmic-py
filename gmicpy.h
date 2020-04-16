ce271ed7868508ae38937cbab0c6eaa8ac4f2d93
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-04-16 21:33:47 +0200

    #35 fixing OS type in gmic.__build__

--- gmicpy.h
+++ gmicpy.h
@@ -41,9 +41,9 @@
 #if cimg_OS==0
 #define OS_type "unknown"
 #elif cimg_OS==1
-#define OS_type "windows"
-#elif cimg_OS==2
 #define OS_type "unix"
+#elif cimg_OS==2
+#define OS_type "windows"
 #endif
 
 #define gmicpy_build_info PyUnicode_FromFormat("zlib_enabled:%d libpng_enabled:%d display_enabled:%d fftw3_enabled:%d libcurl_enabled:%d openmp_enabled:%d cimg_OS:%d OS_type:%s", zlib_enabled, libpng_enabled, display_enabled, fftw3_enabled, libcurl_enabled, cimg_use_openmp, cimg_OS, OS_type)
