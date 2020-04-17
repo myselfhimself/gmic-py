8a7f1216a51ce2f0a2ddabc45e40d5a1195c37d6
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-04-17 18:52:47 +0200

    #23 adding developer daily-reformatting command with unicode funkiness

--- build_tools.bash
+++ build_tools.bash
@@ -100,6 +100,12 @@ function 2_compile () {
     set +x
 }
 
+function 20_reformat_all () {
+    # for daily developer use
+    22_reformat_c_style
+    24_reformat_python_style
+}
+
 function 21_check_c_style () {
     [ -x "$(command -v clang-format)" ] || { echo "Install clang-format for C/C++ formatting check" ; exit 1; }
     clang-format gmicpy.cpp > gmicpy.cpp_formatted
@@ -113,6 +119,7 @@ function 22_reformat_c_style () {
     [ -x "$(command -v clang-format)" ] || { echo "Install clang-format for C/C++ reformatting" ; exit 1; }
     clang-format -i gmicpy.cpp
     clang-format -i gmicpy.h
+    echo 'C/C++ formatting with clang-format ✔️'
 }
 
 function 23_check_python_style () {
@@ -124,6 +131,7 @@ function 23_check_python_style () {
 function 24_reformat_python_style () {
     pip install black
     black setup.py tests/ examples/
+    echo 'Python formatting using black ✔️'
 }
 
 function 33_build_manylinux () {
