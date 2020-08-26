4e3490dfb1cc85a74e0be3392b7234f335af3c3b
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-08-26 18:37:19 +0200

    #45 docs: switching to externalized gmic-sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ master_doc = 'index' # Per https://stackoverflow.com/a/56448499/420684
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'gmicpic']
+extensions = ['recommonmark', 'gmic_sphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
