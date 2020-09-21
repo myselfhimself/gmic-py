f403e0d101c4b2768342c7ea699901e4893109fe
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-09-21 10:58:31 +0200

    #59 WIP skimage I/O support

--- tests/test_gmic_numpy_toolkits.py
+++ tests/test_gmic_numpy_toolkits.py
@@ -10,5 +10,15 @@ from test_gmic_py import (
     assert_non_empty_file_exists,
 )
 
+
+def test_toolkit_gmic_from_scikit():
+    from skimage import data, io, filters
+
+    image = data.coins()
+    edges = filters.sobel(image)
+    io.imshow(edges)
+    io.show()
+
+
 def test_toolkit_gmic_to_scikit():
     pass
