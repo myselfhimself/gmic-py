5381bfc350fc4d06718c0ce69b3f5d122329a8d5
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-09-22 19:39:36 +0200

    #59 importing more skimage data with pooch for advanced pytesting

--- tests/test_gmic_numpy_toolkits.py
+++ tests/test_gmic_numpy_toolkits.py
@@ -13,12 +13,26 @@ from test_gmic_py import (
 
 def test_toolkit_gmic_from_scikit():
     from skimage import data, io, filters
+    from skimage.data import download_all
+    download_all() # grabbing more data for proper 3d conversion testing
 
-    image = data.coins()
-    edges = filters.sobel(image)
-    io.imshow(edges)
+    image = data.lfw_subset()
+    print(image.shape)
+    print(image)
+    io.imshow(image)
     io.show()
+
+    # skimage shape is (height, width, spectrum)
+    # gmic will flip them
+
+    gi = gmic.GmicImage.from_numpy_array(image, deinterleave=True)
+    gmic.run("display", gi)
 
 
 def test_toolkit_gmic_to_scikit():
     pass
+
+
+# TODO remove this
+if __name__ == "__main__":
+    test_toolkit_gmic_from_scikit()
