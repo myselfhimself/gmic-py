aedcd6bb22a0603daee6d1bb15cc0a61568ec015
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-09-24 16:57:54 +0200

    #45 python examples reformatting

--- examples/picsum-photos-montage/make_photo_montage.py
+++ examples/picsum-photos-montage/make_photo_montage.py
@@ -7,13 +7,17 @@ output_filename = "myframe.png"
 max_images = 70
 filenames = []
 for a in range(max_images):
-    url = "https://picsum.photos/{}/{}".format(random.randint(50, 200), random.randint(50,200))
+    url = "https://picsum.photos/{}/{}".format(
+        random.randint(50, 200), random.randint(50, 200)
+    )
     filename = "picsum{}.png".format(a)
     filenames.append(filename)
     myfile = requests.get(url, allow_redirects=True)
-    with open(filename, 'wb') as f:
+    with open(filename, "wb") as f:
         f.write(myfile.content)
 
-gmic_command = "{} frame 3,3,0 frame 3,3,255 montage A display output {}".format(" ".join(filenames), output_filename)
+gmic_command = "{} frame 3,3,0 frame 3,3,255 montage A display output {}".format(
+    " ".join(filenames), output_filename
+)
 
 gmic.run(gmic_command)
