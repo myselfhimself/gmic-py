9aafcc725ba5b26f08d2bfaaaf43979c2f72381b
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-12-16 18:41:19 +0100

    #45 tutorial 3 progress 70% (2)

--- examples/tutorial3-animated-files/tutorial-3-animated-files.py
+++ examples/tutorial3-animated-files/tutorial-3-animated-files.py
@@ -53,18 +53,18 @@ g.run("repeat $! blur[$>] {$>*2} done animate", images_list)
 # gmic https://upload.wikimedia.org/wikipedia/commons/c/cb/2016-09-16_20-30-00_eclipse-lunaire-ann2.gif remove[0] repeat $! blur[$>] {$>*5} stars , done frame 40,3 append_tiles ,4 rotate 90 resize_ratio2d 2100,2970 output flipbook.png
 # Or with PIL sequence editor
 # TODO
-# 
+#
 # # Example of a longer GIF requiring multiple pages
 # gmic.run(
 #     "https://gmic.eu/gallery/img/codesamples_full_1.gif remove[0] repeat $! blur[$>] {$>*5} done frame 40,3 append_tiles 2,2 display rotate 90 resize_ratio2d 2100,2970 display"
 # )
-# 
+#
 # # Example of live window demo
 # gmic.run(
 #     "w[] https://gmic.eu/gallery/img/codesamples_full_1.gif remove[0] repeat $! blur[$>] {$>*5} w[$>] done"
 # )
-# 
+#
 # # ---------------
-# 
+#
 # # TODO AVI non-linear video editor example
 # # Using PyAv https://scikit-image.org/docs/dev/user_guide/video.html#pyav which embed libffmpeg on all OSes
