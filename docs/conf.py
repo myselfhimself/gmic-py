3d5bc096ad378fac665e56d2e2990ed9f3b264e7
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-12-23 20:59:45 +0100

    #71 retriggering doc build

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ from pathlib import Path
 
 project = 'gmic-py'
 copyright = '2020, Jonathan-David Schröder'
-author = 'Jonathan-David Schröder'
+author = 'Jonathan-David Schröder, David Tschumperlé'
 
 # The full version, including alpha/beta/rc tags
 release = Path('../VERSION').read_text()
